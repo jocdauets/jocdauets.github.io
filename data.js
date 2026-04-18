1e7e4492ac253f0c3f97890c2b7cdf2bdef589f7
jocdauets <jocdauets@gmail.com>
2026-04-18 16:23:46 +0200

    Actualització base de dades

--- data.js
+++ data.js
@@ -1,5 +1,42 @@
 _partides = [
 	{
+		"jugadors": ["Jofre", "Roger", "Raúl"],
+		"categories_especials": ["dauet(5)", "4/4"],
+		"puntuacions": {
+			"Jofre": [1,12,21,28,30,18,12,36],
+			"Roger": [3,12,15,24,35,42,15,32],
+			"Raúl": [3,10,15,24,35,24,14,28]
+		},
+		//"classificacio": {},
+		"trumfo": [[], [], [], [], ["tatxar"], [], [], []],
+		"data": "27-01-2026"
+	},
+	{
+		"jugadors": ["Raúl", "Jofre", "Roger"],
+		"categories_especials": ["color", "5/3"],
+		"puntuacions": {
+			"Raúl": [4,12,18,20,25,6,42,29],
+			"Jofre": [5,10,15,28,35,24,36,0],
+			"Roger": [2,8,18,20,30,18,42,69]
+		},
+		//"classificacio": {},
+		"trumfo": [[], [], [], [], [], [], ["tatxar"], []],
+		"data": "27-01-2026"
+	},
+	{
+		"jugadors": ["Roger", "Joanna", "Raúl", "Jofre"],
+		"categories_especials": ["5/3", "dauet"],
+		"puntuacions": {
+			"Roger": [5,10,21,24,35,36,34,0],
+			"Joanna": [3,10,15,24,40,12,0,27],
+			"Raúl": [2,10,12,20,30,12,37,0],
+			"Jofre": [7,16,18,24,35,36,25,13]
+		},
+		//"classificacio": {},
+		"trumfo": [[], [], [], [], ["tatxar-6"], [], [], []],
+		"data": "27-01-2026"
+	},
+	{
 		"jugadors": ["Carla", "Andreu", "Maria", "Dani"],
 		"categories_especials": ["forma", "4/4"],
 		"puntuacions": {
