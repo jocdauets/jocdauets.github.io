cb4db469097f2e659dddacd5fef893655261bf05
jocdauets <jocdauets@gmail.com>
2026-04-22 13:28:30 +0200

    Partides Wesseling League (up to date)

--- data.js
+++ data.js
@@ -7,9 +7,8 @@ _partides = [
 			"Roger": [3,12,15,24,35,42,15,32],
 			"Raúl": [3,10,15,24,35,24,14,28]
 		},
-		//"classificacio": {},
 		"trumfo": [[], [], [], [], ["tatxar"], [], [], []],
-		"data": "27-01-2026"
+		"data": "26-01-2026"
 	},
 	{
 		"jugadors": ["Raúl", "Jofre", "Roger"],
@@ -19,9 +18,8 @@ _partides = [
 			"Jofre": [5,10,15,28,35,24,36,0],
 			"Roger": [2,8,18,20,30,18,42,69]
 		},
-		//"classificacio": {},
 		"trumfo": [[], [], [], [], [], [], ["tatxar"], []],
-		"data": "27-01-2026"
+		"data": "26-01-2026"
 	},
 	{
 		"jugadors": ["Roger", "Joanna", "Raúl", "Jofre"],
@@ -32,9 +30,41 @@ _partides = [
 			"Raúl": [2,10,12,20,30,12,37,0],
 			"Jofre": [7,16,18,24,35,36,25,13]
 		},
-		//"classificacio": {},
 		"trumfo": [[], [], [], [], ["tatxar-6"], [], [], []],
-		"data": "27-01-2026"
+		"data": "31-01-2026"
+	},
+	{
+		"jugadors": ["Raúl", "Jofre", "Roger"],
+		"categories_especials": ["color", "4/4"],
+		"puntuacions": {
+			"Raúl": [7,6,18,20,30,6,31,36],
+			"Jofre": [4,6,18,28,35,18,0,36],
+			"Roger": [4,4,21,28,35,24,29,0]
+		},
+		"trumfo": [[], [], [], [], [], [], ["tatxar-5"], []],
+		"data": "01-02-2026"
+	},
+	{
+		"jugadors": ["Roger", "Raúl", "Jofre"],
+		"categories_especials": ["4/4", "forma"],
+		"puntuacions": {
+			"Roger": [4,4,18,20,30,24,32,32],
+			"Raúl": [3,14,12,24,35,30,28,37],
+			"Jofre": [4,6,18,24,30,18,36,33]
+		},
+		"trumfo": [[], [], [], [], [], [], [], ["tatxar-6"]],
+		"data": "01-02-2026"
+	},
+	{
+		"jugadors": ["Raúl", "Jofre", "Roger"],
+		"categories_especials": ["joc", "dauet(5)"],
+		"puntuacions": {
+			"Raúl": [4,12,15,16,30,30,22,0],
+			"Jofre": [3,10,21,28,25,24,28,13],
+			"Roger": [5,8,21,24,50,30,24,0]
+		},
+		"trumfo": [[], [], ["tatxar"], [], [], [], [], []],
+		"data": "08-02-2026"
 	},
 	{
 		"jugadors": ["Carla", "Andreu", "Maria", "Dani"],
@@ -45,7 +75,6 @@ _partides = [
 			"Maria": [4,4,18,12,30,12,0,36],
 			"Dani": [7,2,15,20,30,36,12,28]
 		},
-		"classificacio": {"Dani": 3, "Andreu": 10, "Carla": 2, "Maria": 0},
 		"trumfo": [[], ["penyora"], [], [], [], [], [], []],
 		"data": "29-03-2026"
 	},
@@ -58,7 +87,6 @@ _partides = [
 			"Maria": [7,8,18,28,35,6,20,31],
 			"Albert": [6,12,15,24,25,24,35,28]
 		},
-		"classificacio": {"Joanna": 10, "Albert": 1, "Teresa": 3, "Maria": 0},
 		"data": "29-03-2026"
 	},
 	{
@@ -70,7 +98,17 @@ _partides = [
 			"Carla": [7,6,21,12,25,24,0,0],
 			"Robert": [5,12,21,28,25,18,0,0]
 		},
-		"classificacio": {"Roger": 2, "Albert": 10, "Carla": 0, "Robert": 1},
 		"data": "29-03-2026"
+	},
+	{
+		"jugadors": ["Jofre", "Roger", "Raúl"],
+		"categories_especials": ["dauet(10)", "color"],
+		"puntuacions": {
+			"Jofre": [7,10,24,24,30,12,16,33],
+			"Roger": [3,10,21,24,55,18,18,39],
+			"Raúl": [4,12,15,28,30,18,17,42]
+		},
+		"trumfo": [[], [], [], [], ["tatxar-3"], [], [], []],
+		"data": "13-04-2026"
 	}
 ]
