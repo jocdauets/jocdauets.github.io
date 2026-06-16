e9d85790f98afdb8e9534944f2628b55c262a181
jocdauets <jocdauets@gmail.com>
2026-06-16 14:11:24 +0200

    Mensa 16/06/26

--- data.js
+++ data.js
@@ -168,7 +168,8 @@ _partides = [
 	{"jugadors":["Giulia","Roger","Jorge"],"categories_especials":["5/3 ","forma"],"puntuacions":{"Giulia":[2,16,15,24,15,24,37,0],"Roger":[4,14,15,16,30,18,37,0],"Jorge":[4,12,9,28,35,12,20,19]},"trumfo":[[""],[""],[""],[""],[""],[""],[""],[""]],"data":"15-6-2026"},
 	{"jugadors":["Anti","Murci","Roger"],"categories_especials":["4/4","color"],"puntuacions":{"Anti":[6,8,18,20,35,30,0,30],"Murci":[1,14,21,24,35,18,40,0],"Roger":[4,8,21,24,55,42,28,23]},"trumfo":[[""],[""],[""],[""],[""],[""],[""],[""]],"data":"15-6-2026"},
 	{"jugadors":["Roger","Giulia"],"categories_especials":["4/4","forma"],"puntuacions":{"Roger":[4,10,24,28,25,30,28,31],"Giulia":[7,10,18,24,30,24,36,30]},"trumfo":[[""],[""],[""],[""],[""],[""],[""],[""]],"data":"15-6-2026"},
-	{"jugadors":["Roger","Giulia"],"categories_especials":["color","dauet(12)"],"puntuacions":{"Roger":[7,14,15,12,25,24,26,32],"Giulia":[2,12,21,24,25,24,40,35]},"trumfo":[[""],[""],[""],[""],[""],[""],[""],[""]],"data":"15-6-2026"}
+	{"jugadors":["Roger","Giulia"],"categories_especials":["color","dauet(12)"],"puntuacions":{"Roger":[7,14,15,12,25,24,26,32],"Giulia":[2,12,21,24,25,24,40,35]},"trumfo":[[""],[""],[""],[""],[""],[""],[""],[""]],"data":"15-6-2026"},
+	{"jugadors":["Murci","Lucas","Jorge"],"categories_especials":["4/4","color"],"puntuacions":{"Murci":[8,8,15,16,30,24,36,33],"Lucas":[3,14,12,24,35,30,36,25],"Jorge":[6,12,21,28,30,24,44,31]},"trumfo":[[""],[""],[""],[""],[""],[""],[""],[""]],"data":"16-6-2026"}
 ]
 
 _tornejos = [
