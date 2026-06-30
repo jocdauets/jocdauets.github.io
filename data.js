5fbde9002423c5007252bb2ff3774263a48d77c5
jocdauets <jocdauets@gmail.com>
2026-06-30 16:31:54 +0200

    Canviat sistema daus

--- data.js
+++ data.js
@@ -188,11 +188,11 @@ _partides = [
 	{"jugadors":["Adri","Carla"],"categories_especials":["2/2/2/2","color"],"puntuacions":{"Adri":[4,12,15,24,30,24,24,24],"Carla":[4,6,18,28,25,12,52,24]},"trumfo":[[""],[""],[""],[""],[""],[""],[""],[""]],"data":"21-6-2026", "sistema_daus": 1},
 	{"jugadors":["Carla","Adri"],"categories_especials":["4/4","color"],"puntuacions":{"Carla":[5,6,18,16,35,12,32,30],"Adri":[5,10,24,20,35,30,36,24]},"trumfo":[[""],[""],[""],[""],[""],[""],[""],[""]],"data":"21-6-2026", "sistema_daus": 1},
 	{"jugadors":["Adri","Carla"],"categories_especials":["dauet(12)","4/4"],"puntuacions":{"Adri":[3,4,15,16,30,24,39,20],"Carla":[5,10,15,24,35,6,0,36]},"trumfo":[[""],[""],[""],[""],[""],[""],[""],[""]],"data":"21-6-2026", "sistema_daus": 1},
-	{"jugadors":["Carla","Teresa","Andreu","Dani","Robert","Roger"],"categories_especials":["4/4","5/3"],"puntuacions":{"Carla":[5,8,24,28,35,36,36,0],"Teresa":[3,10,15,28,35,36,32,37],"Andreu":[7,4,18,28,30,30,36,27],"Dani":[7,14,15,32,25,18,16,37],"Robert":[2,10,12,32,20,36,24,29],"Roger":[4,4,21,24,30,12,24,29]},"trumfo":[[],[],[],[],[],[],[],[]],"data":"23-6-2026","puntua_rdp":true},
-	{"jugadors":["Roger","Joanna"],"categories_especials":["4/4","forma"],"puntuacions":{"Roger":[5,6,9,20,30,36,28,38],"Joanna":[3,6,15,28,35,30,32,26]},"trumfo":[[""],[""],[""],[""],[""],[""],[""],[""]],"data":"25-6-2026"},
-	{"jugadors":["Joanna","Roger"],"categories_especials":["5/3","forma"],"puntuacions":{"Joanna":[5,8,18,24,25,36,30,32],"Roger":[0,8,12,24,35,30,28,32]},"trumfo":[[""],[""],[""],[""],[""],[""],[""],[""]],"data":"25-6-2026"},
-	{"jugadors":["Joanna","Roger"],"categories_especials":["Dau","2/2/2/2"],"puntuacions":{"Joanna":[4,12,18,28,35,42,23,54],"Roger":[4,10,18,24,40,42,20,24]},"trumfo":[[""],[""],[""],[""],[""],[""],[""],[""]],"data":"25-6-2026"},
-	{"jugadors":["Theresa","Joanna"],"categories_especials":["4/4","color"],"puntuacions":{"Theresa":[8,12,18,28,30,18,28,40],"Joanna":[6,12,12,20,40,24,28,31]},"trumfo":[[""],[""],[""],[""],[""],[""],[""],[""]],"data":"27-6-2026"}
+	{"jugadors":["Carla","Teresa","Andreu","Dani","Robert","Roger"],"categories_especials":["4/4","5/3"],"puntuacions":{"Carla":[5,8,24,28,35,36,36,0],"Teresa":[3,10,15,28,35,36,32,37],"Andreu":[7,4,18,28,30,30,36,27],"Dani":[7,14,15,32,25,18,16,37],"Robert":[2,10,12,32,20,36,24,29],"Roger":[4,4,21,24,30,12,24,29]},"trumfo":[[],[],[],[],[],[],[],[]],"data":"23-6-2026","puntua_rdp":true, "sistema_daus": 2},
+	{"jugadors":["Roger","Joanna"],"categories_especials":["4/4","forma"],"puntuacions":{"Roger":[5,6,9,20,30,36,28,38],"Joanna":[3,6,15,28,35,30,32,26]},"trumfo":[[""],[""],[""],[""],[""],[""],[""],[""]],"data":"25-6-2026", "sistema_daus": 2},
+	{"jugadors":["Joanna","Roger"],"categories_especials":["5/3","forma"],"puntuacions":{"Joanna":[5,8,18,24,25,36,30,32],"Roger":[0,8,12,24,35,30,28,32]},"trumfo":[[""],[""],[""],[""],[""],[""],[""],[""]],"data":"25-6-2026", "sistema_daus": 2},
+	{"jugadors":["Joanna","Roger"],"categories_especials":["Dau","2/2/2/2"],"puntuacions":{"Joanna":[4,12,18,28,35,42,23,54],"Roger":[4,10,18,24,40,42,20,24]},"trumfo":[[""],[""],[""],[""],[""],[""],[""],[""]],"data":"25-6-2026", "sistema_daus": 2},
+	{"jugadors":["Theresa","Joanna"],"categories_especials":["4/4","color"],"puntuacions":{"Theresa":[8,12,18,28,30,18,28,40],"Joanna":[6,12,12,20,40,24,28,31]},"trumfo":[[""],[""],[""],[""],[""],[""],[""],[""]],"data":"27-6-2026", "sistema_daus": 2}
 ]
 
 _tornejos = [
@@ -288,8 +288,8 @@ _tornejos = [
 ]
 
 _regions = {
-	"L'Escala": ["Adri", "Dani", "Albert", "Andreu", "Carla", "Teresa", "Maria", "Quim", "Robert"],
-	"Wesseling-Bonn": ["Roger", "Sergio", "Giulia", "Lucas", "Raúl", "Jorge", "Alice", "Diego", "Jofre", "Alejandro", "Joanna", "Anti", "Andrés", "Murci", "Dani OG", "Javi"],
+	"Barna": ["Andreu", "Teresa", "Andreu M", "Míriam", "Aniol", "Quim"],
 	"Lichtenberg": ["Roger", "Theresa", "Justus", "Merle", "Lia", "Joanna", "Jan", "Greta", "Heidi"],
-	"Barna": ["Andreu", "Teresa", "Andreu M", "Míriam", "Aniol", "Quim"]
+	"L'Escala": ["Adri", "Dani", "Albert", "Andreu", "Carla", "Teresa", "Maria", "Quim", "Robert"],
+	"Wesseling-Bonn": ["Roger", "Sergio", "Giulia", "Lucas", "Raúl", "Jorge", "Alice", "Diego", "Jofre", "Alejandro", "Joanna", "Anti", "Andrés", "Murci", "Dani OG", "Javi"]
 }
