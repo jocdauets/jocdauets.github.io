e1743a4b38562bf853b613bd585b9274b94e001e
jocdauets <jocdauets@gmail.com>
2026-07-22 13:24:13 +0200

    Partides 22/07/26

--- data.js
+++ data.js
@@ -275,7 +275,9 @@ _partides = [
 	{"jugadors":["Adri","Anamary","Elena"],"categories_especials":["4/4","buida"],"puntuacions":{"Adri":[3,8,18,20,30,24,32,0],"Anamary":[4,10,15,28,40,24,24,0],"Elena":[7,12,21,32,25,12,32,0]},"trumfo":[[""],[""],[""],[""],[""],[""],[""],[""]],"data":"19-7-2026"},
 	{"jugadors":["Volker","Roger","Joanna","Justus"],"categories_especials":["4/4","color"],"puntuacions":{"Volker":[1,4,15,24,35,30,36,0],"Roger":[2,6,18,24,35,36,24,23],"Joanna":[3,10,21,16,40,18,36,34],"Justus":[2,8,18,28,30,24,32,25]},"trumfo":[[""],[""],[""],[""],[""],[""],[""],[""]],"data":"20-7-2026"},
 	{"jugadors":["Justus","Volker","Helena","Roger","Joanna"],"categories_especials":["4/4","color"],"puntuacions":{"Justus":[5,10,18,24,35,36,40,33],"Volker":[7,14,24,24,30,24,0,0],"Helena":[4,20,18,28,30,30,36,29],"Roger":[6,6,18,28,35,24,36,0],"Joanna":[3,8,12,24,20,6,0,20]},"trumfo":[[""],[""],[""],[""],[""],[""],[""],[""]],"data":"20-7-2026"},
-	{"jugadors":["Volker","Justus","Helena"],"categories_especials":["4/4", "color"],"puntuacions":{"Volker":[1,14,15,20,40,36,0,39],"Justus":[3,4,24,20,25,12,32,35],"Helena":[4,6,15,28,25,24,0,24]},"trumfo":[[""],[""],[""],[""],[""],[""],[""],[""]],"data":"21-7-2026"}
+	{"jugadors":["Volker","Justus","Helena"],"categories_especials":["4/4", "color"],"puntuacions":{"Volker":[1,14,15,20,40,36,0,39],"Justus":[3,4,24,20,25,12,32,35],"Helena":[4,6,15,28,25,24,0,24]},"trumfo":[[""],[""],[""],[""],[""],[""],[""],[""]],"data":"21-7-2026"},
+	{"jugadors":["Txell","Maria","Carla","Adri"],"categories_especials":[],"puntuacions":{"Txell":[7,16,18,24,30,42],"Maria":[2,12,21,28,35,30],"Carla":[7,8,12,24,25,18],"Adri":[5,14,18,28,30,24]},"trumfo":[[""],[""],[""],[""],[""],[""],[""],[""]],"data":"22-7-2026"},
+	{"jugadors":["Txell","Maria","Carla","Adri"],"categories_especials":["5/3","2/2/2/2"],"puntuacions":{"Txell":[6,14,18,20,30,24,30,26],"Maria":[1,4,18,20,35,18,31,28],"Carla":[3,2,18,32,35,18,37,26],"Adri":[5,12,15,28,35,36,37,30]},"trumfo":[[""],[""],[""],[""],[""],[""],[""],[""]],"data":"22-7-2026"}
 ]
 
 _tornejos = [
@@ -374,6 +376,6 @@ _regions = {
 	"Barna": ["Andreu", "Teresa", "Andreu M", "Míriam", "Aniol", "Quim", "Joan", "Aina", "Guerau", "Nona"],
 	"Chicago": ["Dani Ibai"],
 	"Lichtenberg": ["Roger", "Theresa", "Justus", "Merle", "Lia", "Joanna", "Jan", "Greta", "Heidi", "Helena", "Volker", "Nicole"],
-	"L'Escala": ["Adri", "Dani", "Albert", "Andreu", "Carla", "Teresa", "Maria", "Quim", "Robert", "Punky", "RK", "Marta", "Mariona", "Anamary", "Elena", "Oscar", "Drac Català", "Dani H", "Beni", "Dolors", "Nona"],
+	"L'Escala": ["Adri", "Dani", "Albert", "Andreu", "Carla", "Teresa", "Maria", "Quim", "Robert", "Punky", "RK", "Marta", "Mariona", "Anamary", "Elena", "Oscar", "Drac Català", "Dani H", "Beni", "Dolors", "Nona", "Txell"],
 	"Wesseling-Bonn": ["Roger", "Sergio", "Giulia", "Lucas", "Raúl", "Jorge", "Alice", "Diego", "Jofre", "Alejandro", "Joanna", "Anti", "Andrés", "Murci", "Dani OG", "Javi", "Lucía", "Pedro", "Miguel", "Max", "Dani Ibai", "Ioanna", "Charls", "Йордан"]
 }
